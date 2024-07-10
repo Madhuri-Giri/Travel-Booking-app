1ae761933d89251f6ff499843dfb14fdd00d6013
Tanisha Patidar <tanishapatidar905@gmail.com>
2024-07-10 19:12:16 +0530

    bus page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import BusSearch from './components/bus/BusSearch'
-import HotelSearch from './components/hotel/HotelSearch'
 
 const App = () => {
   return (
-    <div>
-        <Routes>
-         <Route path='/bus-search' element={<BusSearch/>} /> 
-         <Route path='/hotel-search' element={<HotelSearch/>} /> 
-
-        </Routes>
-    </div>
+    <div>App</div>
   )
 }
 
