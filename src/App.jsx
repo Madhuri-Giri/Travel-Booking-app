6f1f4a1c70bf06a65c9714e878f3914e7a2f97bb
Sabnam09 <sabnambegam9597@gmail.com>
2024-07-12 10:57:32 +0530

    flightBooking

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BusSearch from './components/bus/BusSearch'
 import HotelSearch from './components/hotel/HotelSearch'
+import FlightSearch from './components/flight/FlightSearch'
+import Footer from './pages/footer/Footer';
+import CustomNavbar from './pages/navbar/CustomNavbar';
 
 const App = () => {
   return (
     <div>
       <Router>
+        <CustomNavbar/>       
         <Routes>
+          <Route path='/' element={<FlightSearch />} />
           <Route path='/bus-search' element={<BusSearch />} />
           <Route path='/hotel-search' element={<HotelSearch />} />
         </Routes>
+        <Footer/>        
       </Router>
     </div>
   )
