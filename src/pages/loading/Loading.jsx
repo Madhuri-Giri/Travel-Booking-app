a8b689fcf2d2115ec9b6407b440f89ccc0d7bbd5
Tanisha Patidar <tanishapatidar905@gmail.com>
2024-08-08 12:26:37 +0530

    new loader

--- src/pages/loading/Loading.jsx
+++ src/pages/loading/Loading.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import loaderGif from "../../assets/images/lg.gif"
 import "./Loading.css"
 
 const Loading = () => {
   return (
     <div className='loading'>
-       <img style={{width:"50%"}} src={loaderGif} alt="Loading..." className="loading-gif" />
+       <img src={loaderGif} alt="Loading..." className="loading-gif" />
     </div>
   )
 }
