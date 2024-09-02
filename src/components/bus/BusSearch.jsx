3b79c12d7bdcee7c7bc0f7bf821356eae75e0681
Madhuri-Giri <madhurigoswami23475@gmail.com>
2024-09-02 19:11:59 +0530

    remove filled

--- src/components/bus/BusSearch.jsx
+++ src/components/bus/BusSearch.jsx
@@ -1,6 +1,7 @@
+
 import { useState, useRef, useEffect} from 'react';
 
-import { useState, useRef, useEffect } from 'react';
+
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
--- src/redux-toolkit/store/store.js
+++ src/redux-toolkit/store/store.js
@@ -2,13 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import busReducer from "../slices/busSlice"
 import profileReducer from "../slices/profileSlice"
 import  hotelSearchReducer from "../slices/hotelSlice"
-
+import timerReducer from "../slices/timerSlice"
 const store = configureStore ({
           reducer:{
                     bus: busReducer,
 
                     profile: profileReducer,   
                     hotelSearch: hotelSearchReducer,
+                    timer: timerReducer,
 
 
           }
