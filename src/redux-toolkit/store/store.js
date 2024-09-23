b92d99282a995648be8ca11acf2f6237b8798092
Sabnam <sabnambegam9597@gmail.com>
2024-09-23 13:22:36 +0530

    all erros done

--- src/redux-toolkit/store/store.js
+++ src/redux-toolkit/store/store.js
@@ -3,10 +3,9 @@ import busReducer from "../bus/busSlice"
 import profileReducer from "../slices/profileSlice"
 import hotelSearchReducer from "../slices/hotelSlice"
 import timerReducer from "../slices/timerSlice"
-import  hotelSearchReducer from "../slices/hotelSlice"
 import hotelDetailsReducer from "../slices/hotelInfoSlice"
 import hotelRoomsReducer from "../slices/hotelRoomSlice"
-import  hotelBlockReducer from '../slices/hotelBlockSlice'
+import hotelBlockReducer from '../slices/hotelBlockSlice'
 import bookingReducer from "../slices/hotelBookSlice"
 import busListReducer from "../bus/busListSlice"
 
@@ -17,13 +16,34 @@ import busSelectionReducer from "../bus/busSelectionSlice"
 import borddropReducer from "../../redux-toolkit/bus/borddropSlice"
 import loginSlice from '../slices/loginSlice'
 
+// const store = configureStore({
+//     reducer: {
+//         bus: busReducer,
+//         busList: busListReducer,
+//         profile: profileReducer,
+//         hotelSearch: hotelSearchReducer,
+//         timer: timerReducer,
+//         seatLayout: seatlayoutReducer,
+//         selectedSeats: selectedSeatsReducer,
+//         boarding: boardingReducer,
+//         busSelection: busSelectionReducer,
+//         borddrop: borddropReducer,
+//         loginReducer: loginSlice,
+//     }
+
 const store = configureStore({
+
     reducer: {
+
         bus: busReducer,
         busList: busListReducer,
         profile: profileReducer,
-        hotelSearch: hotelSearchReducer,
         timer: timerReducer,
+        hotelSearch: hotelSearchReducer,
+        hotelDetails: hotelDetailsReducer,
+        hotelRooms: hotelRoomsReducer,
+        hotelBlock: hotelBlockReducer,
+        booking: bookingReducer,
         seatLayout: seatlayoutReducer,
         selectedSeats: selectedSeatsReducer,
         boarding: boardingReducer,
@@ -31,33 +51,6 @@ const store = configureStore({
         borddrop: borddropReducer,
         loginReducer: loginSlice,
     }
-
-const store = configureStore ({
-
-          reducer:{
-
-                    bus: busReducer,
-                    busList: busListReducer,
-
-                    profile: profileReducer,   
-                    timer: timerReducer,
-
-
-                    hotelSearch: hotelSearchReducer,
-                    hotelDetails: hotelDetailsReducer,
-                    hotelRooms: hotelRoomsReducer,
-                    hotelBlock:  hotelBlockReducer,
-                    booking: bookingReducer,
-
-          
-
-                    seatLayout: seatlayoutReducer,
-                    selectedSeats: selectedSeatsReducer,
-                    boarding: boardingReducer,
-                    busSelection: busSelectionReducer,
-                    borddrop: borddropReducer,
-                    login: loginSlice,
-                    }
 
 })
 export default store;
